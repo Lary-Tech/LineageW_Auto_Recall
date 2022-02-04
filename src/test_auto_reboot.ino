6f0615346fe451c86b33f314790384d114ff469d
YJack0000 <jackleagends@gmail.com>
2022-02-04 22:20:53 +0800

    RESET_TIME -> unsigned long

--- src/test_auto_reboot.ino
+++ src/test_auto_reboot.ino
@@ -1,4 +1,5 @@
-#define RESET_TIME 180                         // 定義幾秒之後 reset
+//多久之後重新設定
+unsigned long RESET_TIME = 180;
 
 //記錄開始時間
 unsigned long start_time = millis();
@@ -7,20 +8,24 @@ unsigned long start_time = millis();
 void(* resetFunc) (void) = 0;
 
 void setup() {
-  pinMode(13, OUTPUT);    // sets the digital pin 13 as output
-  digitalWrite(13, HIGH); // sets the digital pin 13 on
+  // 開著預設燈號
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  delay(1000);
+  digitalWrite(13, HIGH);
 }
 
 void loop() {
+  // 現在時間
   unsigned long current_time = millis();
-  
+
   // start_time > current_time 代表連續執行超過50天 start_time 歸 0
   // current_time - start_time 為執行時間 如果大於設定的 RESET_TIME 也要重製
   if(start_time > current_time || current_time - start_time >= RESET_TIME*1000){
-    // 重置
-    resetFunc();
     // 以防萬一在初始化一次
     start_time = millis();
+    // 重置
+    resetFunc();
   }
 
   delay(1);
