ac921e203a314a0da9f83cdd4429715d80bb6a60
YJack0000 <jackleagends@gmail.con>
2022-02-24 18:40:35 +0800

    v3

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,8 +1,3 @@
-#include <ld3320.h>
-
-//定義一個語音辨識對
-VoiceRecognition Voice;
-
 //定義重新開機
 void(* resetFunc) (void) = 0;
 
@@ -10,54 +5,66 @@ void(* resetFunc) (void) = 0;
 unsigned long start_time = 0;
 
 //多久之後重新設定
-unsigned long RESET_TIME = 180;
+unsigned long RESET_TIME = 8;
 
 //定義寫入按鍵
 uint8_t buf[8] = { 0 };
+void clickPrtScn();
 void clickF12();
 void releaseKey();
 
 void setup(){
   Serial.begin(9600);
-  
-  // 初始化 ld3320
-  Voice.init(MIC);
-  Voice.addCommand("zao shou di ren gong chi le", 0); 
-  Voice.micVol(100);      
-  Voice.voiceMaxLength(22);              
-  Voice.start();
-
   start_time = millis();
+
+  pinMode(13, OUTPUT);
+  while(analogRead(0) > 10){
+    digitalWrite(13, HIGH);
+  }
+  digitalWrite(13, LOW);
 }
 
-
 void loop() {
-  // 如果 read 到 0
-  switch(Voice.read())
-  {
-    case 0:
-      clickF12();
-      releaseKey();
-      break;
-    default:
-      unsigned long current_time = millis();
-      // start_time > current_time 代表連續執行超過50天 start_time 歸 0
-      // current_time - start_time 為執行時間 如果大於設定的 RESET_TIME 也要重製
-      if(start_time > current_time || current_time - start_time >= RESET_TIME*1000){
-          // 重置
-          resetFunc();
-      }
-      break;
+  int val = analogRead(0);
+  //Serial.println(val);
+  
+  if(val > 11){
+    clickPrtScn();
+    clickF12();
+    delay(1000);
+  }
+  
+  unsigned long current_time = millis();
+  // start_time > current_time 代表連續執行超過50天 start_time 歸 0
+  // current_time - start_time 為執行時間 如果大於設定的 RESET_TIME 也要重製
+  if(start_time > current_time || current_time - start_time >= RESET_TIME*1000){
+      // 重置
+      resetFunc();
   }
 }
 
+void clickPrtScn(){
+  digitalWrite(13, HIGH);
+  //按下按鈕
+  buf[2] = 70;
+  Serial.write(buf, 8);
+  
+  //持續 0.1s
+  delay(100);
+  releaseKey();
+  digitalWrite(13, LOW);
+}
+
 void clickF12(){
+  digitalWrite(13, HIGH);
   //按下按鈕
   buf[2] = 69;
   Serial.write(buf, 8);
   
-  //持續 1s
-  delay(1000);
+  //持續 0.1s
+  delay(100);
+  releaseKey();
+  digitalWrite(13, LOW);
 }
 
 void releaseKey(){
