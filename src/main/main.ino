730057622772dea0f6b87e8d25e7d4e2cbaf6ab0
YJack0000 <jackleagends@gmail.com>
2022-03-01 15:20:08 +0800

    keyboard 獨立宣告

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,77 +1,29 @@
-//定義重新開機
-void(* resetFunc) (void) = 0;
+#include <avr/wdt.h>
+#include "keyboard.h"
 
-//記錄開始時間
-unsigned long start_time = 0;
-
-//多久之後重新設定
-unsigned long RESET_TIME = 8;
-
-//定義寫入按鍵
-uint8_t buf[8] = { 0 };
-void clickPrtScn();
-void clickF12();
-void releaseKey();
+Keyboard keyboard;
 
 void setup(){
   Serial.begin(9600);
-  start_time = millis();
-
-  pinMode(13, OUTPUT);
-  while(analogRead(0) > 10){
-    digitalWrite(13, HIGH);
-  }
-  digitalWrite(13, LOW);
+  wdt_enable(WDTO_2S); 
 }
 
 void loop() {
-  int val = analogRead(0);
-  //Serial.println(val);
+  int val0 = analogRead(0);
+  int val1 = analogRead(1);
+  int val2 = analogRead(2);
   
-  if(val > 11){
+  if(val0 > 500){
     delay(random(100, 500));
-    clickPrtScn();
+    keyboard.clickPrtScn();
+    keyboard.clickF12();
+  }
+
+  if(val1 > 500){
     delay(random(100, 500));
-    clickF12();
-    delay(1000);
   }
-  
-  unsigned long current_time = millis();
-  // start_time > current_time 代表連續執行超過50天 start_time 歸 0
-  // current_time - start_time 為執行時間 如果大於設定的 RESET_TIME 也要重製
-  if(start_time > current_time || current_time - start_time >= RESET_TIME*1000){
-      // 重置
-      resetFunc();
+
+  if(val2 > 500){
+    delay(random(100, 500));
   }
-}
-
-void clickPrtScn(){
-  digitalWrite(13, HIGH);
-  //按下按鈕
-  buf[2] = 70;
-  Serial.write(buf, 8);
-  
-  //持續 0.1s
-  delay(100);
-  releaseKey();
-  digitalWrite(13, LOW);
-}
-
-void clickF12(){
-  digitalWrite(13, HIGH);
-  //按下按鈕
-  buf[2] = 69;
-  Serial.write(buf, 8);
-  
-  //持續 0.1s
-  delay(100);
-  releaseKey();
-  digitalWrite(13, LOW);
-}
-
-void releaseKey(){
-  //釋放按鈕
-  buf[0] = 0;
-  buf[2] = 0;
-  Serial.write(buf, 8);
 }
