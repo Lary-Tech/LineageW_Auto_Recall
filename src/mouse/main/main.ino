5fa4b6912a2bcae2232c2f1906221b2541f31879
YJack0000 <jackleagends@gmail.con>
2022-03-24 01:19:45 +0800

    確定一次祝瞬會成功

--- src/mouse/main/main.ino
+++ src/mouse/main/main.ino
@@ -10,140 +10,71 @@ uint8_t nullReport[4] = { 0, 0, 0, 0 };
 void setup()
 {
     Serial.begin(9600);
-}
-
-/* Move the mouse in a clockwise square every 5 seconds */
-void loop()
-{
-    int ind;
-    delay(1000);
  
     mouseReport.buttons = 0;
     mouseReport.x = 0;
     mouseReport.y = 0;
     mouseReport.wheel = 0;
+}
 
-    // reset
-    mouseReport.x = -64;
-    mouseReport.y = -64;
-    mouseReport.buttons = 0;
-    for (ind = 0; ind < 100; ind++) {
-      Serial.write((uint8_t *) &mouseReport, 4);
-      Serial.write((uint8_t *) &nullReport, 4);
-    }
+void loop()
+{
+    reset_mouse();
+    change_place();
+    delay(5000);
+}
 
-    /*
-    // Go to 祝瞬 約1780 150
-    mouseReport.x = 50;
-    mouseReport.y = 0;
-    mouseReport.buttons = 0;
-    for (ind = 0; ind < 71; ind++) {
-      Serial.write((uint8_t *) &mouseReport, 4);
-      Serial.write((uint8_t *) &nullReport, 4);
-    }
-    mouseReport.x = 0;
-    mouseReport.y = 50;
-    mouseReport.buttons = 0;
-    for (ind = 0; ind < 6; ind++) {
-      Serial.write((uint8_t *) &mouseReport, 4);
-      Serial.write((uint8_t *) &nullReport, 4);
-    }
-    mouseReport.buttons = 1;
+void change_place(){
+  delay(100);
+  mouse(40, 70, 10);
+  click(4);
+  click(1);
+  mouse(18, 12, 10);
+  click(1);
+}
+
+void reset_mouse(){
+  mouseReport.x = -64;
+  mouseReport.y = -64;
+  mouseReport.buttons = 0;
+  
+  for (int i = 0; i < 100; i++) {
     Serial.write((uint8_t *) &mouseReport, 4);
     Serial.write((uint8_t *) &nullReport, 4);
+  }
 
-    delay(1000);
-    */
-    
-    // reset
-    mouseReport.x = -64;
-    mouseReport.y = -64;
+  mouseReport.buttons = 0;
+  mouseReport.x = 0;
+  mouseReport.y = 0;
+  mouseReport.wheel = 0;
+}
+
+void mouse(int8_t x, int8_t y, int t){    
+    mouseReport.x = x;
+    mouseReport.y = y;
     mouseReport.buttons = 0;
-    for (ind = 0; ind < 100; ind++) {
+    for (int i = 0; i < t; i++) {
       Serial.write((uint8_t *) &mouseReport, 4);
       Serial.write((uint8_t *) &nullReport, 4);
     }
-    
-    // Go to 選地圖 約200 330
-    mouseReport.x = 10;
-    mouseReport.y = 0;
+
     mouseReport.buttons = 0;
-    for (ind = 0; ind < 40; ind++) {
-      Serial.write((uint8_t *) &mouseReport, 4);
-      Serial.write((uint8_t *) &nullReport, 4);
-    }
     mouseReport.x = 0;
-    mouseReport.y = 10;
-    mouseReport.buttons = 0;
-    for (ind = 0; ind < 70; ind++) {
-      Serial.write((uint8_t *) &mouseReport, 4);
-      Serial.write((uint8_t *) &nullReport, 4);
-    }
-    
-    mouseReport.buttons = 4;
+    mouseReport.y = 0;
+    mouseReport.wheel = 0;
+
+    delay(100);
+}
+
+void click(uint8_t cl){
+    mouseReport.buttons = cl;
     Serial.write((uint8_t *) &mouseReport, 4);
     Serial.write((uint8_t *) &nullReport, 4);
-    mouseReport.buttons = 0;
     
-    mouseReport.buttons = 1;
-    Serial.write((uint8_t *) &mouseReport, 4);
-    Serial.write((uint8_t *) &nullReport, 4);
-
-    delay(1000);
-
-    // reset
-    mouseReport.x = -64;
-    mouseReport.y = -64;
     mouseReport.buttons = 0;
-    for (ind = 0; ind < 100; ind++) {
-      Serial.write((uint8_t *) &mouseReport, 4);
-      Serial.write((uint8_t *) &nullReport, 4);
-    }
-
-    // Go to 飛 約342 404
-    mouseReport.x = 10;
+    mouseReport.x = 0;
     mouseReport.y = 0;
-    mouseReport.buttons = 0;
-    for (ind = 0; ind <67; ind++) {
-      Serial.write((uint8_t *) &mouseReport, 4);
-      Serial.write((uint8_t *) &nullReport, 4);
-    }
-    mouseReport.x = 0;
-    mouseReport.y = 10;
-    mouseReport.buttons = 0;
-    for (ind = 0; ind < 87; ind++) {
-      Serial.write((uint8_t *) &mouseReport, 4);
-      Serial.write((uint8_t *) &nullReport, 4);
-    }
-    //mouseReport.buttons = 1;
-    //Serial.write((uint8_t *) &mouseReport, 4);
-    //Serial.write((uint8_t *) &nullReport, 4);
+    mouseReport.wheel = 0;
 
-    delay(1000);
-
-    /*
-    // Going up
-    mouseReport.x = 0;
-    mouseReport.y = -2;
-    for (ind = 0; ind < 20; ind++) {
-      Serial.write((uint8_t *)&mouseReport, 4);
-    Serial.write((uint8_t *)&nullReport, 4);
-    }
-
-    // Going right
-    mouseReport.x = 2;
-    mouseReport.y = 0;
-    for (ind = 0; ind < 20; ind++) {
-    Serial.write((uint8_t *)&mouseReport, 4);
-    Serial.write((uint8_t *)&nullReport, 4);
-    }
-
-    // Going down
-    mouseReport.x = 0;
-    mouseReport.y = 2;
-    for (ind = 0; ind < 20; ind++) {
-    Serial.write((uint8_t *)&mouseReport, 4);
-    Serial.write((uint8_t *)&nullReport, 4);
-    }
-    */
+    delay(100);
 }
