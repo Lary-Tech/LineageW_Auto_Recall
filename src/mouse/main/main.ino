e5fa239c241fadb284e01be0dc73a54e25b4155a
YJack0000 <jackleagends@gmail.con>
2022-03-30 00:58:09 +0800

    new mouse

--- src/mouse/main/main.ino
+++ src/mouse/main/main.ino
@@ -17,28 +17,115 @@ void setup()
     mouseReport.wheel = 0;
 }
 
+int c = 0;
+
 void loop()
 {
     reset_mouse();
-    change_place();
-    delay(5000);
+
+    //int val0 = analogRead(0);
+    //int val1 = analogRead(1);
+    //int val2 = analogRead(2);
+
+    //Serial.println(val0);
+    //Serial.println(val1);
+    //Serial.println(val2);
+
+    bool be_attacked = ( analogRead(0) > 500 ? true : false );   // A25 遭受敵人攻擊了
+    bool hp_not_enough = ( analogRead(1) > 500 ? true : false );   // A26 體力不足了
+    bool need_potion = ( analogRead(2) > 500 ? true : false );   // A27 需要補充藥水
+    
+    if( be_attacked ){
+        delay(random(100, 500));
+        //Serial.println("val0: ");
+        recall();
+        reset_mouse();
+        change_place();
+    }
+
+    if( hp_not_enough ){
+        delay(random(100, 500));
+        //Serial.println("val1: ");
+        hp(); // 按鍵6 的強效藥水 
+    }
+
+    if( need_potion ){
+        delay(random(100, 500));
+        //Serial.println("val2:");
+        buy_supply();
+        reset_mouse();
+        change_place();
+    }
+}
+
+void recall(){
+  delay(100);
+  mouse(60, 60, 30);
+  click(4); // 中鍵
+}
+
+void hp(){
+  delay(100);
+  mouse(105, 60, 30);
+  click(1);
+}
+
+void buy_supply(){
+  delay(100);
+  mouse(115, 60, 30);
+  click(1);
+  delay(100);
+  mouse(-5, -8, 30);
+  click(1);
+  delay(100);
+  mouse(-40, -18, 30);
+  click(1);
+  delay(100);
+  mouse(4, 0, 30);
+  click(1);
+
+  delay(10000);
+  
+  delay(random(100, 500));
+  mouse(33, 28, 30);
+  click(1);
+  
+  delay(100);
+  mouse(10, 0, 30);
+  click(1);
+  delay(100);
+  mouse(0, -57, 30);
+  click(1);
 }
 
 void change_place(){
+  delay(100); // 點按叫出儲存的東西
+  mouse(117, 10, 30);
+  click(1);
+
+  reset_mouse();
+
   delay(100);
   mouse(40, 70, 10);
-  click(4);
   click(1);
-  mouse(18, 12, 10);
+  
+  delay(100);
+  
+  mouse(14, 9, 10);
+  click(1);
+
+  delay(1000);
+
+  mouse(93, 18, 30);
   click(1);
 }
 
 void reset_mouse(){
-  mouseReport.x = -64;
-  mouseReport.y = -64;
+  mouseReport.x = -120;
+  mouseReport.y = -120;
   mouseReport.buttons = 0;
   
-  for (int i = 0; i < 100; i++) {
+  for (int i = 0; i < 50; i++) {
     Serial.write((uint8_t *) &mouseReport, 4);
     Serial.write((uint8_t *) &nullReport, 4);
   }
@@ -68,6 +155,19 @@ void mouse(int8_t x, int8_t y, int t){
 
 void click(uint8_t cl){
     mouseReport.buttons = cl;
+    Serial.write((uint8_t *) &mouseReport, 4);
+    Serial.write((uint8_t *) &nullReport, 4);
+    
+    mouseReport.buttons = 0;
+    mouseReport.x = 0;
+    mouseReport.y = 0;
+    mouseReport.wheel = 0;
+
+    delay(100);
+}
+
+void wheel(int8_t wh){
+    mouseReport.wheel = wh;
     Serial.write((uint8_t *) &mouseReport, 4);
     Serial.write((uint8_t *) &nullReport, 4);
     
