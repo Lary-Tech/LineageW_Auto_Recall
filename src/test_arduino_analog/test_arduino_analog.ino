b34390135d7a39d45dd8b8c1515813ac8666f49e
YJack0000 <jackleagends@gmail.con>
2022-04-28 18:14:00 +0800

    test analog

---
+++ src/test_arduino_analog/test_arduino_analog.ino
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int be_attacked    = analogRead(0);
+  int hp_not_enough  = analogRead(1);
+  int need_potion    = analogRead(2);
+  int en_be_attacked = analogRead(3);
+  int en_need_potion = analogRead(4);
+
+  String myString = "be_attack: ";
+  Serial.println(myString + be_attacked);
+
+  myString = "hp_not_enough: ";
+  Serial.println(myString + hp_not_enough);
+  
+  myString = "need_potion: ";
+  Serial.println(myString + need_potion);
+
+  myString = "en_be_attacked: ";
+  Serial.println(myString + en_be_attacked);
+
+  myString = "en_need_potion";
+  Serial.println(myString + en_need_potion);
+
+  delay(1000);
+}
