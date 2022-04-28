4b75eb5bd24e468d3e04fec71b3c72032a7e5e2f
YJack0000 <jackleagends@gmail.con>
2022-04-28 15:48:04 +0800

    v5

---
+++ src/v5/mouse.cpp
@@ -0,0 +1,71 @@
+#include <Arduino.h>
+#include "mouse.h"
+
+Mouse::Mouse(){
+    mouseReport.buttons = 0;
+    mouseReport.x = 0;
+    mouseReport.y = 0;
+    mouseReport.wheel = 0;
+}
+
+Mouse::~Mouse(){
+}
+
+void Mouse::reset_mouse(){
+  mouseReport.x = -120;
+  mouseReport.y = -120;
+  mouseReport.buttons = 0;
+  
+  for (int i = 0; i < 50; i++) {
+    Serial.write((uint8_t *) &mouseReport, 4);
+    Serial.write((uint8_t *) &nullReport, 4);
+  }
+
+  mouseReport.buttons = 0;
+  mouseReport.x = 0;
+  mouseReport.y = 0;
+  mouseReport.wheel = 0;
+}
+
+void Mouse::move(int8_t x, int8_t y, int t){    
+    mouseReport.x = x;
+    mouseReport.y = y;
+    mouseReport.buttons = 0;
+    for (int i = 0; i < t; i++) {
+      Serial.write((uint8_t *) &mouseReport, 4);
+      Serial.write((uint8_t *) &nullReport, 4);
+    }
+
+    mouseReport.buttons = 0;
+    mouseReport.x = 0;
+    mouseReport.y = 0;
+    mouseReport.wheel = 0;
+
+    delay(100);
+}
+
+void Mouse::click(uint8_t cl){
+    mouseReport.buttons = cl;
+    Serial.write((uint8_t *) &mouseReport, 4);
+    Serial.write((uint8_t *) &nullReport, 4);
+    
+    mouseReport.buttons = 0;
+    mouseReport.x = 0;
+    mouseReport.y = 0;
+    mouseReport.wheel = 0;
+
+    delay(500);
+}
+
+void Mouse::wheel(uint8_t wh){
+    mouseReport.wheel = wh;
+    Serial.write((uint8_t *) &mouseReport, 4);
+    Serial.write((uint8_t *) &nullReport, 4);
+    
+    mouseReport.buttons = 0;
+    mouseReport.x = 0;
+    mouseReport.y = 0;
+    mouseReport.wheel = 0;
+
+    delay(500);
+}
---
+++ src/v5/place.h
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+struct place_func{
+    void(*func)(int);
+    int arg;
+};
+
+enum change_flag{ DO_NOT_CHANGE_PLACE, DO_CHANGE_PLACE };
+
+class Place{
+    public:
+        Place();
+        ~Place();
+        void execute(change_flag);
+        void execute(int);
+    private:
+        place_func place_array[10];
+        int n_place;
+        int count_place;
+
+        void insert_place(void(*)(int), int);
+};
