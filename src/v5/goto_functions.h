900b437925927c6483a787d5b9ed338f63c7c7e2
YJack0000 <jackleagends@gmail.con>
2022-04-29 13:30:41 +0800

    fix expected initialize

--- src/v5/goto_functions.h
+++ src/v5/goto_functions.h
@@ -2,6 +2,6 @@
 #define GOTO_FUNCTIONS_H
 
 void Aden1_3F(int); //<<<<<<<<<<<<<<<<<<<<<<  1 <= place <= 3
-void v4sDungeon1_3F(int) //<<<<<<<<<<<<<<<<<<<<<<  1 <= place <= 3
+void v4sDungeon1_3F(int); //<<<<<<<<<<<<<<<<<<<<<<  1 <= place <= 3
 
 #endif
