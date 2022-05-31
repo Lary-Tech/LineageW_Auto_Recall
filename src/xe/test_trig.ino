767e3d8390d5fdc391ebc1dbf63bd5d7660b9a5b
YJack0000 <jackleagends@gmail.com>
2022-05-31 22:07:03 +0800

    digit 7

--- src/xe/test_trig.ino
+++ src/xe/test_trig.ino
@@ -1,6 +1,8 @@
 void setup()
 {
     Serial.begin(9600);
+
+    pinMode(7, INPUT); 
 }
 
 void loop(){
@@ -10,7 +12,7 @@ void loop(){
     bool en_be_attacked = ( analogRead(3) > 500 ? true : false ); // B0 English 遭受敵人攻擊
     bool en_need_potion = ( analogRead(4) > 500 ? true : false );   // B1 English 需要補充藥水
     bool en_hp_not_enough = ( analogRead(5) > 500 ? true : false );   // B2 English 體力不足了
-    bool trigger = analogRead(6);   // 聽聲音瞬間觸發
+    bool trigger = digitalRead(7);   // 聽聲音瞬間觸發
 
     Serial.println(trigger);
 }
